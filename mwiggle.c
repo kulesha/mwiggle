c2530c6b2bb0d02ea10e7dc7a6c4c7ad700ace1a
Eugene Kulesha <kulesha@gmail.com>
2013-12-05 11:05:41 +0000

    added update command to updated meta info about the file and the its tracks

--- mwiggle.c
+++ mwiggle.c
@@ -7,7 +7,7 @@ Contact: kulesh@gmail.com
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "mw.h"
 
 void usage( void ) {
@@ -48,7 +48,8 @@ void print_stats(char *fname) {
     i = 0;
     do {
       t = tracks+i;
-      printf("Track %d: %s\n", t->id, t->name);
+      printf("Track ID: %d\n", t->id);
+      printf("\t Name : %s\n", t->name);
       printf("\t Strand : %c\n", (t->ori > 0) ? '+' : '-');
       printf("\t Values : %f .. %f\n", t->min, t->max);
       printf("\t Desc   : %s\n", t->desc);
@@ -151,6 +152,167 @@ int dump_data(char *fname, char **opts) {
   return 0;
 }
 
+int update_meta(char *fname, char **opts) {
+  char *tracks = NULL;
+  FILE *f;
+  HEADER h;
+  int taxid;
+
+  TRACK *ltracks = NULL;
+  REGION *regions = NULL;
+
+  int i = 0;
+
+  if ( (f = fopen(fname, "r"))) {
+    if (fread(&h, sizeof(HEADER), 1, f)) {
+      ltracks =  (TRACK*) malloc(sizeof(TRACK) * h.tracks );
+      for (i = 0; i<h.tracks; i++) {
+	if ( !fread((ltracks+i), sizeof(TRACK), 1, f)) {
+	  printf("Error: could not read from %s\n", fname);
+	  fclose(f);
+	  free(ltracks);
+	  return -1;
+	} 
+      }
+
+      regions =  (REGION*) malloc(sizeof(REGION) * (h.regions+1) );
+      for (i = 0; i<h.regions; i++) {
+	if ( !fread((regions+i), sizeof(REGION), 1, f)) {
+	  printf("Error: could not read from %s\n", fname);
+	  fclose(f);
+	  return -1;
+	}
+      }
+      (regions+h.regions)->size = 0; // to indicate the end of the list
+
+    } else {
+      printf("Error: could not read %s\n", fname);
+      fclose(f);
+      return -1;
+    }
+    
+    fclose(f);
+  }
+
+  char *name = NULL, *info = NULL;
+  UCHAR ori = -1;
+
+  while (*opts) {
+    if ((*opts)[0] == '-') {
+      switch((*opts)[1]) {
+      case 't': // tracks, e.g -t 0,2
+	opts++;
+	tracks = *opts;	
+	break;
+      case 'n': // name, e.g -n Study1
+	opts++;
+	name = *opts;
+	break;
+      case 's': // strand, e.g -s +
+	opts++;
+	if (*opts[0] == '-') {
+	  ori = 0;
+	}  else {
+	  ori = 1;
+	}
+	break;
+      case 'i': // strand, e.g -i "this track is for tissue 1"
+	opts++;
+	info = *opts;
+	break;
+
+      case 'd': // description, e.g -d "My first study"
+	opts++;
+	sprintf(h.desc, *opts);
+	break;
+      case 'a': // assembly, e.g -a NCBI37
+	opts++;
+	sprintf(h.assembly, *opts);
+	break;
+      case 'x': // tax id, e.g -x 9606
+	opts++;
+	if ((taxid = atoi(*opts)) > 0) {
+	  h.taxon = taxid;
+	} else {
+	  printf("Error: invalid taxonomy id [%s]\n", *opts);
+	}
+	break;
+      default:
+	printf("Error: unknown option -%c\n", (*opts)[1]);
+	return -1;
+      }
+    } 
+    opts++;
+  }
+
+
+  if (tracks) {
+    UCHAR tarray[MAX_TRACKS];
+    const char s[2] = ",";
+    int tnum, active = -1;
+    char *token;
+
+    memset(tarray, 1, MAX_TRACKS);  
+    // tracks are specified
+    if ((token = strtok(tracks, s))) {
+      if ((strchr(token, '-') == NULL)) { // - means all tracks
+	memset(tarray, 0, MAX_TRACKS);
+	tnum = atoi(token);
+	tarray[tnum] = 1;
+	active = 1;
+	while (token != NULL) {
+	  token = strtok(NULL, s);
+	  if (token) {
+	    tnum = atoi(token);
+	    tarray[tnum] = 1;
+	    active ++;
+	  }
+	}
+      }
+    }
+
+    for (i = 0; i<h.tracks; i++) {
+      TRACK *tmp = (ltracks + i);
+      if (tarray[ tmp->id ]) {
+	if (name) {
+	  sprintf(tmp->name, name);
+	}
+	if (info) {
+	  sprintf(tmp->desc, info);
+	}
+	if (ori >= 0) {
+	  tmp->ori = ori;
+	}
+
+      }
+    }
+  }
+
+
+  if ((f = fopen(fname,"r+"))) {
+    TRACK *t;
+
+    fseek(f, 0 , SEEK_SET);
+    fwrite(&h, sizeof(HEADER), 1, f);
+
+    if (tracks) {
+      for (i = 0; i<h.tracks; i++) {
+	t = (ltracks + i);
+	fwrite(t, sizeof(TRACK), 1, f);
+      } 
+    }
+    free(ltracks);
+    free(regions);
+    fclose(f);
+  } else {
+    printf("Error: failed to open %s for writing\n", fname);
+  }
+
+
+
+  return 0;
+}
+
 int main (int argc, char **argv) {
   char *cmd;
   char *fname;
@@ -181,6 +343,9 @@ int main (int argc, char **argv) {
     break;
   case 'd': // dump
     dump_data(fname, argv);
+    break;
+  case 'u': // update
+    update_meta(fname, argv);
     break;
   default:
     printf("Error: Unrecognized command: %s\n", cmd);
